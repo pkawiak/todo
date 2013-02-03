b665a1913cfedb7071aad5559937e326b9df8f82
Piotr Kawiak <piotr.kawiak@gmail.com>
2013-02-03 14:37:26 +0100

    Yet another rake attempt to compile coffeescript

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,18 @@
-task :default => [:coffee]
+require 'coffee-script'
 
-task :coffee do
- system "coffee --join public/scripts/application.js --compile coffee"
+namespace :js do
+  desc "compile coffee-scripts from ./coffee to ./public/scripts"
+  task :compile do
+    source = "#{File.dirname(__FILE__)}/coffee/"
+    javascripts = "#{File.dirname(__FILE__)}/public/scripts/"
+
+    Dir.foreach(source) do |cf|
+      unless cf == '.' || cf == '..' 
+        js = CoffeeScript.compile File.read("#{source}#{cf}") 
+        open "#{javascripts}#{cf.gsub('.coffee', '.js')}", 'w' do |f|
+          f.puts js
+        end 
+      end 
+    end
+  end
 end
