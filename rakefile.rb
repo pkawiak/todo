94194d48a6a159d0b8f01fcb9c09e65414004a21
Piotr Kawiak <piotr.kawiak@gmail.com>
2013-02-03 15:02:45 +0100

    Yet another rake attempt to compile coffeescript

--- rakefile.rb
+++ rakefile.rb
@@ -6,9 +6,10 @@ namespace :js do
     source = "#{File.dirname(__FILE__)}/coffee/"
     javascripts = "#{File.dirname(__FILE__)}/public/scripts/"
     FileUtils.mkdir_p javascripts
-
+    puts " >>>>>>>>>>>>>> 1"
     Dir.foreach(source) do |cf|
-      unless cf == '.' || cf == '..' 
+      unless cf == '.' || cf == '..'
+        puts " >>>>>>>>>>>>>> 2"
         js = CoffeeScript.compile File.read("#{source}#{cf}") 
         open "#{javascripts}#{cf.gsub('.coffee', '.js')}", 'w' do |f|
           f.puts js
